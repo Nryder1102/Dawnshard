242c4e20473651c5bd02c6364237c2efe9bfe88b
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-11-20 07:10:23 +0100

    Switch to custom FrozenKeyedCollection instead of KeyedCollection

--- DragaliaAPI.Shared/MasterAsset/MasterAssetData.cs
+++ DragaliaAPI.Shared/MasterAsset/MasterAssetData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Frozen;
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Text.Json;
@@ -26,12 +27,12 @@ public class MasterAssetData<TKey, TItem>
 
     private readonly string jsonFilename;
     private readonly Func<TItem, TKey> keySelector;
-    private readonly Lazy<InternalKeyedCollection> internalKeyCollection;
+    private readonly FrozenKeyedCollection internalKeyCollection;
 
     /// <summary>
     /// Gets a <see cref="IEnumerable{TItem}"/> of all the collection's values.
     /// </summary>
-    public IEnumerable<TItem> Enumerable => this.internalKeyCollection.Value.AsEnumerable();
+    public IEnumerable<TItem> Enumerable => this.internalKeyCollection.Items;
 
     /// <summary>
     /// Get a <typeparam name="TItem"> instance corresponding to the given <typeparam name="TKey"/> key.</typeparam>
@@ -58,7 +59,7 @@ public class MasterAssetData<TKey, TItem>
     /// <param name="key">The key to index with.</param>
     /// <returns>The returned value.</returns>
     /// <exception cref="KeyNotFoundException">The given key was not present in the collection.</exception>
-    public TItem this[TKey key] => this.internalKeyCollection.Value[key];
+    public TItem this[TKey key] => this.internalKeyCollection[key];
 
     /// <summary>
     /// Attempts to get a <typeparam name="TItem"> instance corresponding to the given <typeparam name="TKey"/> key.</typeparam>
@@ -67,7 +68,7 @@ public class MasterAssetData<TKey, TItem>
     /// <param name="item">The returned value, if the master data contained it.</param>
     /// <returns>A bool indicating whether the value was successfully retrieved.</returns>
     public bool TryGetValue(TKey key, [NotNullWhen(true)] out TItem? item) =>
-        this.internalKeyCollection.Value.TryGetValue(key, out item);
+        this.internalKeyCollection.TryGetValue(key, out item);
 
     /// <summary>
     /// Creates a new instance of <see cref="MasterAssetData{TKey,TItem}"/>.
@@ -79,42 +80,34 @@ public class MasterAssetData<TKey, TItem>
     {
         this.jsonFilename = jsonFilename;
         this.keySelector = keySelector;
-        this.internalKeyCollection = new(DataFactory);
+        this.internalKeyCollection = DataFactory();
     }
 
-    private InternalKeyedCollection DataFactory()
+    private FrozenKeyedCollection DataFactory()
     {
-        InternalKeyedCollection result = new(this.keySelector);
         string path = Path.Join(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
             JsonFolder,
             jsonFilename
         );
 
-        IEnumerable<TItem> items =
-            JsonSerializer.Deserialize<IEnumerable<TItem>>(
+        IReadOnlyCollection<TItem> items =
+            JsonSerializer.Deserialize<IReadOnlyCollection<TItem>>(
                 File.ReadAllText(path),
                 MasterAssetJsonOptions.Instance
             ) ?? throw new JsonException("Deserialized IEnumerable was null");
 
-        foreach (TItem i in items)
-            result.Add(i);
-
-        return result;
+        return new FrozenKeyedCollection(items, this.keySelector);
     }
 
-    private class InternalKeyedCollection : KeyedCollection<TKey, TItem>
+    private class FrozenKeyedCollection(IReadOnlyCollection<TItem> items, Func<TItem, TKey> selector)
     {
-        private readonly Func<TItem, TKey> keySelector;
+        public readonly FrozenSet<TItem> Items = items.ToFrozenSet();
+        private readonly FrozenDictionary<TKey, TItem> dictionary = items.ToFrozenDictionary(selector, x => x);
 
-        public InternalKeyedCollection(Func<TItem, TKey> keySelector)
-        {
-            this.keySelector = keySelector;
-        }
+        public TItem this[TKey key] => this.dictionary[key];
 
-        protected override TKey GetKeyForItem(TItem item)
-        {
-            return this.keySelector.Invoke(item);
-        }
+        public bool TryGetValue(TKey key, [NotNullWhen(true)] out TItem? item) =>
+            this.dictionary.TryGetValue(key, out item);
     }
 }
